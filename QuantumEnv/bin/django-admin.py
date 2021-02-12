159921634c84c107acda3171f199f50b8fd646c3
Matt Crook <matt.crook11@gmail.com>
2021-02-12 17:48:04 -0600

    fix: remove unwanted tracked files.

--- QuantumEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/matthewcrook/code/nss/frontEnd/quantumapp/QuantumEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
