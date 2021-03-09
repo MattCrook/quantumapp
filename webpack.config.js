d1dbc53d87885a21a209302d14312bc2d8c2c25a
Matt Crook <matt.crook@getbuilt.com>
2021-03-09 11:46:53 -0600

    webpack config for quantumadmin fix.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleTracker = require('webpack-bundle-tracker')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
-
-const NAMES = {
-  images: "images",
-  assets: "assets",
-  static: "static",
-};
-
-const ASSETS = {
-  images: path.join(NAMES.assets, NAMES.images),
-  static: path.join(NAMES.static, NAMES.assets, NAMES.images),
-};
-
-
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleTracker = require("webpack-bundle-tracker");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// outputPath is place where your want to save files
+// publicPath is what url you have in js, css and etc files.
 module.exports = {
-  mode: 'development',
-  entry: { quantumadminapp: "./quantumadminapp/src/index.js" },
-  devtool: 'inline-source-map',
+  mode: "development",
+  context: path.resolve(__dirname, "quantumadminapp/"),
+  entry: "./src/index.js",
+  output: {
+    pathinfo: true,
+    path: path.resolve("./quantumadminapp/static"),
+    // filename: "[name].js", // Emit app.js by capturing entry name
+    filename: "js/bundle.js",
+    chunkFilename: 'js/[name].chunk.js',
+    // clean: true,
+    // publicPath: "static/app/",
+    publicPath: "/",
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
+  devtool: "inline-source-map",
   devServer: {
     contentBase: path.join(__dirname, "dist/"),
     port: 8080,
@@ -29,22 +32,21 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Quantum Coasters Admin',
+      title: "Quantum Coasters Admin",
     }),
   ],
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve('./quantumadminapp/static/app/'),
-    filename: "[name].js", // Emit app.js by capturing entry name
-    // path: path.join(__dirname, "static"), // Output to dist directory
-    clean: true,
-    publicPath: "static/app/",
-  },
+  // resolve: { extensions: ["*", ".js", ".jsx"] },
+  // output: {
+  //   path: path.resolve("./quantumadminapp/static/app/"),
+  //   filename: "[name].js", // Emit app.js by capturing entry name
+  //   clean: true,
+  //   publicPath: "static/app/",
+  // },
   plugins: [
     new CleanWebpackPlugin(),
     new BundleTracker({
       path: __dirname,
-      filename: './webpack-stats.json',
+      filename: "./webpack-stats.json",
     }),
   ],
   module: {
@@ -61,6 +63,17 @@ module.exports = {
         },
       },
       {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
         test: /\.css$/i,
         use: [
           {
@@ -73,78 +86,51 @@ module.exports = {
               url: true,
             },
           },
+          // {
+          //   loader: "resolve-url-loader",
+          //   options: {
+          //     sourceMap: true,
+          //   },
+          // },
+        ],
+      },
+      {
+        // test: /\.(gif|png|jpe?g|svg)$/i,
+        // type: 'asset/resource',
+        test: /\.(png|jp(e*)g|svg)$/,
+        exclude: /node_modules/,
+        use: [
           {
-            loader: "resolve-url-loader",
+            loader: "url-loader",
             options: {
-              sourceMap: true,
+              limit: 8000,
+              name: 'static/media/[name].[hash:8].[ext]',
+              // name: "assets/[name].[md5:hash:hex:8].[ext]",
+              // name: "media/[hash]-[name].[ext]",
+              // encoding: "utf8",
             },
           },
         ],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
-        type: 'asset/resource',
+        exclude: /node_modules/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "[name].[ext]",
+              name: 'static/media/[name].[hash:8].[ext]',
+              // name: "assets/[name].[md5:hash:hex:8].[ext]",
+              // name: "media/[hash]-[name].[ext]",
+              // path: path.resolve(__dirname, "images"),
               // path: path.join(__dirname, "static", "images"),
               // outputPath: ASSETS.images,
               // publicPath: ASSETS.static,
               // outputPath: "images",
-              // publicPath: "images",
-
-
-            },
-          },
-          {
-            loader: "url-loader",
-            options: {
-              encoding: "utf8",
             },
           },
         ],
       },
-
-      // {
-      //   test: /\.(png|jpg|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       options: {
-      //         // path: path.resolve(__dirname, "dist/"),
-      //         // path: "/static/media",
-      //         outputPath: ASSETS.images,
-      //         // publicPath: "/static/media",
-      //         name: "[path][name].[ext]",
-      //       },
-      //     },
-      //   ],
-      // },
-      // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       query:{
-      //         outputPath: './media/',
-      //         name: '[name].[ext]?[hash]'
-      //     }
-      //     },
-      //   ],
-      // },
-      // {
-      //   test: /\.svg$/i,
-      //   use: [
-      //     {
-      //       loader: "url-loader",
-      //       options: {
-      //         encoding: "utf8",
-      //       },
-      //     },
-      //   ],
-      // },
       {
         test: /\.txt$/i,
         use: "raw-loader",
@@ -152,67 +138,3 @@ module.exports = {
     ],
   },
 };
-
-
-
-
-
-
-
-
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const ImageminPlugin = require('imagemin-webpack-plugin').default;
-// const CopyPlugin = require('copy-webpack-plugin');
-
-// const PATHS = {
-//   // when using __dirname, resolve and join gives same result,
-//   // because __dirname is absolute path to directory of this file.
-//   // OK to use no slashes,
-//   // both resolve and join adds platform-specific separators by default
-//   src: path.resolve(__dirname, "src"),
-//   dist: path.resolve(__dirname, "dist"),
-//   build: path.resolve(__dirname, "build"),
-//   test: path.resolve(__dirname, "test"),
-// };
-
-// const NAMES = {
-//   // JS FILES
-//   index: "index",
-//   print: "print",
-//   // Chrome Extension Development
-//   popup: "popup",
-//   options: "options",
-//   background: "background",
-//   contentScript: "contentScript",
-
-//   // FOLDERS
-//   assets: "assets",
-//   utilities: "utilities",
-//   images: "images",
-//   fonts: "fonts",
-//   include: "include",
-// };
-
-// const FILE_PATHS = {
-//   // JS
-//   indexJs: `${path.join(PATHS.src, NAMES.index)}.js`,
-//   printJs: `${path.join(PATHS.src, NAMES.print)}.js`,
-//   // Chrome Extension Development
-//   popupJs: `${path.join(PATHS.src, NAMES.popup)}.js`,
-//   optionsJs: `${path.join(PATHS.src, NAMES.options)}.js`,
-//   backgroundJs: `${path.join(PATHS.src, NAMES.background)}.js`,
-//   contentScriptJs: `${path.join(PATHS.src, NAMES.include, NAMES.contentScript)}.js`,
-
-//   // HTML
-//   indexHtml: `${path.join(PATHS.src, NAMES.index)}.html`,
-//   printHtml: `${path.join(PATHS.src, NAMES.print)}.html`,
-//   // Chrome Extension Development
-//   popupHtml: `${path.join(PATHS.src, NAMES.popup)}.html`,
-//   optionsHtml: `${path.join(PATHS.src, NAMES.options)}.html`,
-//   backgroundHtml: `${path.join(PATHS.src, NAMES.background)}.html`,
-// };
-
-// const ASSETS = {
-//   images: path.join(NAMES.assets, NAMES.images),
-//   fonts: path.join(NAMES.assets, NAMES.fonts),
-// };
